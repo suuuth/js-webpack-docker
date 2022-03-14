1b26f99484fc659f409b757ce09378233cb8519b
kevin@aenow.com <kevin@aenow.com>
2022-03-14 10:10:04 -0400

    file cleanup

--- public/css/config/app.config.js
+++
@@ -1,5 +0,0 @@
-
-import 'main/css/source/variables.css';
-import 'main/css/source/browser-reset.css';
-import 'main/css/source/spacing.css';
-import 'main/css/source/utility.css';
---
+++ public/js/config/app.config.js
@@ -0,0 +1,2 @@
+
+import 'main/js/source/App.vue';
